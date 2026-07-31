c7ea1ddc486ca56e756e13726ec9288bf61dce73
Prathima <perikalaprathima@gmail.com>
2026-07-31 21:54:36 +0530

    Added namespace and generic TypeScript programs

---
+++ genericVariable.ts
@@ -0,0 +1,9 @@
+// Generic variable
+
+let numberValue: number = 100;
+let stringValue: string = "Hello";
+let booleanValue: boolean = true;
+
+console.log(numberValue);
+console.log(stringValue);
+console.log(booleanValue);
